d2b840cb1cdb476e89d0e4e7120819d5ea69e596
Paddy Urlich <paddyurlich@gmail.com>
2017-01-11 13:25:33 +1300

    added percentage delta to revenue

--- table_cluster_3G.php
+++ table_cluster_3G.php
@@ -23,9 +23,9 @@
               $decPlaces = $fieldtype3G[$key]['dec'];
               $arrowType = $fieldtype3G[$key]['arrow'];
               $KPI = $fieldtype3G[$key]['alias'];
-              $pre = (float)($stats_3G_pre_cluster['pre'][0][$key]);
-              $post = (float)($stats_3G_post_cluster_and_newSite['post'][0][$key]);
-              $delta = (float)($post - $pre); 
+              $pre = number_format((float)($stats_3G_pre_cluster['pre'][0][$key]),$decPlaces);
+              $post = number_format((float)($stats_3G_post_cluster_and_newSite['post'][0][$key]),$decPlaces);
+              $delta = number_format((float)($post - $pre),$decPlaces); 
 
               // SET ARROW DIRECTION = NORMAL
               
@@ -66,13 +66,24 @@
                 }
 
                 $glyph = " <span class='glyphicon glyphicon-arrow-".$arrow."' style='color:".$arrow_color."'></span>";
-          
+                
+                if ($delta > 0) { 
+                  $percentage_delta = number_format((($post-$pre)/$pre)*100,2);
+                }
+
                 echo "<tr>";
                   echo "<th>".$KPI."</th>";
-                  echo "<td>".number_format($pre,$decPlaces)."</th>";        
-                  echo "<td>".number_format($post,$decPlaces)."</th>"; 
-                  echo "<td>".number_format($delta,$decPlaces)."</th>"; 
-                  echo "<td>".$glyph."</th>";        
+                  // echo "<td>".number_format($pre,$decPlaces)."</th>";       
+                  echo "<td>".$pre."</th>";  
+                  echo "<td>".$post."</th>"; 
+                  echo "<td>".$delta."</th>"; 
+                  
+                  if ($table == "traffic") {
+                    echo "<td>".$glyph."<span style='font-size:75%;color:".$arrow_color.";'>"." (".$percentage_delta." %)</span></th>";        
+                  } else {
+                      echo "<td>".$glyph."</th>";
+                  }
+
                 echo "</tr>";
             }
       ?>
