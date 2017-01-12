122e2d5bddb9b092288bd6034bbcdffd8925c5cb
Patrick Urlich <paddyurlich@Patricks-MacBook-Pro-2.local>
2017-01-13 11:20:06 +1300

    added index php redirect and config php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+
+
+
+function connection(){
+    
+    $servername = "172.21.200.37";
+    $username = "patrickurlich";
+    $password = "forPUonly";
+    $dbname = "ranPU";
+    $table = "Acceptance_Stats_3G_daily";
+
+    // Create connection
+    //$connect = new mysqli($servername, $username, $password, $dbname);
+     $connect = mysqli_connect($servername, $username,$password,$dbname); 
+    // Check connection
+    if ($connect->connect_error) {
+        die("Connection failed: " . $connect->connect_error);
+    }     
+}
+
+?>
